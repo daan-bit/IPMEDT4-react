bbe8ef0607519b3d68699ae3b9e42e52832138a6
daan-bit <s1119605@student.hsleiden.nl>
2021-05-26 10:59:30 +0200

    react werkend

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class App extends React.Component{
+    
+    render(){
+        return(
+            <section>
+                <h1>Hallo</h1>
+                <p>Dit is een test</p>
+            </section>
+            
+        );
+    }
+    
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+//altijd 4 stappen om werkend
+
+//1. react zelf importeren
+import React from "react";
+
+//2. react dom importeren
+import ReactDOM from "react-dom";
+
+//3. app
+import App from "./App";
+
+//4. App component in DOM schieten
+ReactDOM.render(<App />, document.getElementById("root"));
