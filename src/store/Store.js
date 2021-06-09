7b24a627ef6c62662c695165929f0fdfb9774eb2
prgnl <alexfilinsky@gmail.com>
2021-06-09 14:08:05 +0200

    redux aangepast
    met dank aan slimme daniel

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,9 +1,20 @@
 import {combineReducers, createStore} from "redux";
-import {verwijder, show} from "../store/reducers/Reducers";
+import {verwijder, show, AuthReducer} from "../store/reducers/Reducers";
+
+const initialStates = {
+    auth: {
+        loggedIn: false,
+        user: {} //empty object creeeren als initiele state
+    },
+    //hier maken we meerdere states aan, denk aan bijvoorbeeld onderzoek state
+}
 
 export const store = createStore(
     combineReducers({
         verwijder,
         show,
-    })
+        auth:AuthReducer,
+        initialStates,
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
--- src/store/reducers/Reducers.js
+++ src/store/reducers/Reducers.js
@@ -17,3 +17,15 @@ export const show = (state = "", action) => {
             return state;
     }
 };
+
+
+export const AuthReducer = (state = {}, actions) => {
+    switch (actions.type) {
+        case 'SET_LOGIN': //case nodig bij Login.js
+            return {...state, loggedIn: true, user: actions.payload};
+        case "SET_LOGOUT":     //uitloggen
+        return {...state, loggedIn: false, user: {}};
+        default:
+            return state;
+    }
+};
