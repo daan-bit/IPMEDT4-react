f9faa0880feb4e603c9e22b0a04fc1a680260658
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-06-21 23:19:35 +0200

    Revert "Merge branch 'daniek_dev' into development"
    This reverts commit 883d89c2caea66c64f240bcf2cd3e1b564547741, reversing
    changes made to 0a5a308da603ceef4d3d6ad9d43cddcbf6636ff2.

---
+++ src/components/test-login/test-login.js
@@ -0,0 +1,12 @@
+import React,  { Component } from 'react';
+import Navbar from './navbar';
+class Test extends Component {
+
+    render() {
+        return(
+           <Navbar />
+        );
+    }
+};
+
+export default Test
